7253189d9513ca1fca89cb003719a1ad30893111
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-01-22 12:28:37 +0800

    Create prob2.go

---
+++ problems/prob2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    str := "racecar"
+    fmt.Println(isPalindrome(str))
+}
+
+func isPalindrome(str string) bool {
+    for i := 0; i < len(str)/2; i++ {
+        if str[i] != str[len(str)-i-1] {
+            return false
+        }
+    }
+    return true
+}
